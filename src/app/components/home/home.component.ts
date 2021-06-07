f3f227739dd58b07fd7b994c3ad46c945e035ad6
Patryk Starzycki <patryks9090@gmail.com>
2021-06-07 15:06:21 +0200

    filtering by categories

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,8 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {ProductService} from "../../service/product.service";
 import {createProductArray, Product} from "../../models/product";
 import {Subscription} from "rxjs";
 import {MatTableDataSource} from "@angular/material/table";
+import {CategoriesService} from "../../service/categories.service";
+import {ActivatedRoute} from "@angular/router";
 
 @Component({
   selector: 'app-home',
@@ -11,15 +13,31 @@ import {MatTableDataSource} from "@angular/material/table";
 })
 export class HomeComponent implements OnInit {
 
+  categories$
   products: Product[] = [];
-  subscription: Subscription;
+  subscription: Subscription | undefined;
+  category: string | null = '';
+  filteredProduct: Product[] | undefined;
 
-  constructor( private productService:ProductService) {
+  constructor(private productService: ProductService,
+              private categoryService: CategoriesService,
+              private route: ActivatedRoute) {
+
+    this.categories$ = categoryService.getAll();
     this.subscription = this.productService.getAll().subscribe(products => {
       this.products = createProductArray(products);
+      route.queryParamMap.subscribe(params => {
+        this.category = params.get('category');
+        this.filteredProduct = this.category ? this.products.filter(p => p.category == this.category) : this.products;
+      });
     });
+
+
   }
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+
+  }
+
 
 }
